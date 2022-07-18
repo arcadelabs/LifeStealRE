9f49b750ba035c2b3ff58fc9a9993b10b4eeff30
RONiN <69498033+RealRONiN@users.noreply.github.com>
2022-07-18 08:56:03 +0530

    fix: giveheart command

--- src/main/java/in/arcadelabs/lifesteal/commands/GiveHearts.java
+++ src/main/java/in/arcadelabs/lifesteal/commands/GiveHearts.java
@@ -23,24 +23,44 @@ import in.arcadelabs.labaide.libs.aikar.acf.annotation.CommandAlias;
 import in.arcadelabs.labaide.libs.aikar.acf.annotation.CommandCompletion;
 import in.arcadelabs.labaide.libs.aikar.acf.annotation.CommandPermission;
 import in.arcadelabs.labaide.libs.aikar.acf.annotation.Subcommand;
+import in.arcadelabs.labaide.libs.aikar.acf.bukkit.contexts.OnlinePlayer;
 import in.arcadelabs.lifesteal.LifeSteal;
 import in.arcadelabs.lifesteal.LifeStealPlugin;
+import in.arcadelabs.lifesteal.hearts.HeartItemManager;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Map;
 
 @CommandAlias("lifesteal|ls")
-@CommandPermission("lifesteal.gethearts")
+@CommandPermission("lifesteal.givehearts")
 public class GiveHearts extends BaseCommand {
 
   private final LifeSteal lifeSteal = LifeStealPlugin.getLifeSteal();
+  private HeartItemManager heartItemManager;
+  private ItemStack replacementHeart;
 
-  /**
-   * On give hearts command.
-   *
-   * @param sender the sender
-   * @param hearts the hearts
-   */
-  @Subcommand("gethearts")
-  @CommandCompletion("@players")
-  public void onGiveHearts(final CommandSender sender, final int hearts) {
+  @Subcommand("givehearts")
+  @CommandCompletion("@players Blessed|Normal|Cursed @nothing")
+  public void onGiveHearts(final CommandSender sender, final OnlinePlayer target, final String type, final int amount) {
+    switch (type) {
+      case "Blessed" -> giveHearts("blessed", HeartItemManager.Mode.RANDOM_BLESSED, target.player, amount);
+      case "Normal" -> giveHearts("normal", HeartItemManager.Mode.RANDOM_NORMAL, target.player, amount);
+      case "Cursed" -> giveHearts("cursed", HeartItemManager.Mode.RANDOM_CURSED, target.player, amount);
+    }
+  }
+
+  public void giveHearts(final String type, final HeartItemManager.Mode mode, final Player player, final int amount) {
+    heartItemManager = new HeartItemManager(mode)
+            .prepareIngedients()
+            .cookHeart();
+    replacementHeart = heartItemManager.getHeartItem();
+    replacementHeart.setAmount(amount);
+
+    final Map<Integer, ItemStack> items = player.getInventory().addItem(replacementHeart);
+    for (final Map.Entry<Integer, ItemStack> leftovers : items.entrySet()) {
+      player.getWorld().dropItemNaturally(player.getLocation(), leftovers.getValue());
+    }
   }
 }
