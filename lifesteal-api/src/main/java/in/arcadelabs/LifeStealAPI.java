853b0047ede893fb5eaae6c1c6627d96a91a408b
RONiN <69498033+RealRONiN@users.noreply.github.com>
2022-09-08 15:56:48 +0530

    build: core & api modules

---
+++ lifesteal-api/src/main/java/in/arcadelabs/LifeStealAPI.java
@@ -0,0 +1,30 @@
+/*
+ *          LifeSteal - Yet another lifecore smp core.
+ *                Copyright (C) 2022  Arcade Labs
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package in.arcadelabs;
+
+import in.arcadelabs.lifesteal.database.profile.StatisticsManager;
+import in.arcadelabs.lifesteal.hearts.HeartItemManager;
+
+public interface LifeStealAPI {
+
+  StatisticsManager getStatisticsManager();
+
+  HeartItemManager getHeartItemManager();
+
+}
